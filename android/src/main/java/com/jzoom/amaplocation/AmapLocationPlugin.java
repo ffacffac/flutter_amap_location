0f61182358fab932ce02bb719ef66769ff56d698
huangqj <huangqj@huaweisoft.com>
2022-06-22 09:38:24 +0800

    添加隐私接口调用

--- android/src/main/java/com/jzoom/amaplocation/AmapLocationPlugin.java
+++ android/src/main/java/com/jzoom/amaplocation/AmapLocationPlugin.java
@@ -221,10 +221,15 @@ public class AmapLocationPlugin implements MethodCallHandler, AMapLocationListen
 
     private boolean startup(Map arguments) {
         synchronized (this){
-
+			AMapLocationClient.updatePrivacyShow(getApplicationContext(), true, true);
+            AMapLocationClient.updatePrivacyAgree(getApplicationContext(), true);
             if(locationClient==null){
                 //初始化client
-                locationClient = new AMapLocationClient(getApplicationContext());
+                try {
+                    locationClient = new AMapLocationClient(getApplicationContext());
+                } catch (Exception e) {
+                    return false;
+                }
                 //设置定位参数
                 AMapLocationClientOption option = new AMapLocationClientOption();
                 parseOptions(option,arguments);
